5f3ee21779399f016c0e7c8bc6efcd30e809b2c9
Cho-HyeonDeok <103423802+Cho-HyeonDeok@users.noreply.github.com>
2023-12-04 21:29:43 +0900

    Revert "20231204 8시43분"

--- MoviePort/src/main/java/com/movieport/service/MypageService.java
+++ MoviePort/src/main/java/com/movieport/service/MypageService.java
@@ -1,9 +1,6 @@
 package com.movieport.service;
 
-import java.util.List;
-
 import com.movieport.model.MemberVO;
-import com.movieport.model.ReviewVO;
 
 public interface MypageService {
 	// 회원정보 불러오기
@@ -17,17 +14,6 @@ public interface MypageService {
 
 	// 회원탈퇴
 	public void deleteMember(MemberVO member) throws Exception;
-
-	// 내가 작성 리뷰 리스트 불러오기
-	public List<ReviewVO> getMyReviewList(String id) throws Exception;
-
-	// 리뷰 상세
-	public ReviewVO getMyReviewListDetail(int reviewid) throws Exception;
-
-	// 리뷰 수정
-	public int updateReview(ReviewVO review) throws Exception;
-
-	// 리뷰 삭제
-	public void deleteReview(ReviewVO review) throws Exception;
+	
 
 }
