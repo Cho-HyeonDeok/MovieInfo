b739b9f3b84e6275f0eeb3a22f642c23662bc620
admin <admin@0-0>
2023-12-04 20:43:30 +0900

    20231204 8시43분

--- MoviePort/src/main/java/com/movieport/mapper/MypageMapper.java
+++ MoviePort/src/main/java/com/movieport/mapper/MypageMapper.java
@@ -1,6 +1,9 @@
 package com.movieport.mapper;
 
+import java.util.List;
+
 import com.movieport.model.MemberVO;
+import com.movieport.model.ReviewVO;
 
 public interface MypageMapper {
 	// 회원정보 불러오기
@@ -14,4 +17,16 @@ public interface MypageMapper {
 
 	// 회원탈퇴
 	public void deleteMember(MemberVO member);
+	
+	// 내가 작성 리뷰 리스트 불러오기
+	public List<ReviewVO> getMyReviewList(String id);
+	
+	// 리뷰 상세
+	public ReviewVO getMyReviewListDetail(int reviewid);
+	
+	// 리뷰 수정
+	public int updateReview(ReviewVO review);
+	
+	// 리뷰 삭제
+	public void deleteReview(ReviewVO review);
 }
